36fca63171a05b54e622dee00f54585d860e84bd
nzixuan <nzixuan@gmail.com>
2022-06-27 14:58:30 +0800

    fix: audit trail page fix

--- frontend/src/components/admin/AuditTrailView.js
+++ frontend/src/components/admin/AuditTrailView.js
@@ -26,7 +26,7 @@ export default function AuditTrailView(props) {
     const [lazyParams, setLazyParams] = useState({
         first: 0,
         rows: 10,
-        page: 1,
+        page: 0,
     });
 
 
@@ -37,6 +37,7 @@ export default function AuditTrailView(props) {
     const loadLazyData = () => {
         setLoading(true);
         axios.get(process.env.REACT_APP_API_URL + '/api/edboard/audit', { params: { auditPerPage: lazyParams.rows, page: lazyParams.page } }).then((res) => {
+            console.log(res.data)
             setTotalRecords(res.data.total_result);
             setAudits(res.data.audits)
             setLoading(false);
