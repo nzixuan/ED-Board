549395ac1c82214a6d8c36546ef5ab4b8b2ecd9a
nzixuan <nzixuan@gmail.com>
2022-07-08 17:52:13 +0800

    fix: header class name error

--- frontend/src/components/admin/PreviewAddTabs.js
+++ frontend/src/components/admin/PreviewAddTabs.js
@@ -16,28 +16,3 @@ export default function PreviewAddTabs(props) {
     </TabView>)
 
 }
-
-// function ETable(props) {
-//     return (
-//         <div className="p-2">
-//             {
-//                 props.roster &&
-//                 < DataTable value={props.roster["roster"]} header={(props.roster.staffType).toUpperCase()} responsiveLayout="scroll"
-//                     showGridlines rowGroupMode="rowspan" groupRowsBy="assignment" size="small" rowClassName={(data) => { return data.stripe }}>
-//                     <Column className="py-2 px-1 font-bold " field="assignment" header="Assignment" headerClassName="header"></Column>
-//                     <Column className="py-1 px-1" header="AM" body={nameTemplate("am")} headerClassName="header"></Column>
-//                     {props.roster.staffType === "doctor" &&
-//                         <Column className="py-1 px-1" header="Straddle" body={nameTemplate("straddle1")} headerClassName="header"></Column>
-//                     }
-//                     <Column className="py-1 px-1" header="PM" body={nameTemplate("pm")} headerClassName="header"></Column>
-//                     {props.roster.staffType === "doctor" &&
-//                         <Column className="py-1 px-1" header="Straddle" body={nameTemplate("straddle2")} headerClassName="header"></Column>
-//                     }
-//                     <Column className="py-1 px-1" header="ND" body={nameTemplate("nd")} headerClassName="header"></Column>
-//                 </DataTable>
-//             }
-//         </div >
-
-
-//     )
-// }
